9b3cb572379d2b993551979004cf5ffd522072c5
motsubele96 <motsubelekb@gmail.com>
2024-04-17 12:23:47 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Defines a JSON-to-object function."""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
+
+
+0x0B-python-input_output/5-save_to_json_file.py
+
+#!/usr/bin/python3
+
+"""Defines a JSON file-writing function."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as f:
