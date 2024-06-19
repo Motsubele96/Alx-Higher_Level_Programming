662274c5c0cdd4fb44af0dbfc672a52f236aa4d9
Motsubele96 <motsubelekb@gmail.com>
2024-06-19 07:36:42 -0700

    ./2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if ( process.argv.length === 2 ) {
+	console.log ('no argument');
+} else if ( process.argv.length === 3) {
+	console.log ('Argument Found');
+} else {
+	console.log ('Arguments Found');
+}
