960ae22b0a64a7000519af6e02689667a2ee6fd9
Motsubele96 <motsubelekb@gmail.com>
2024-06-19 07:14:33 -0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,5 @@
 #!/user/bin/node
-console.log('C is fun');
+//console.log('C is fun');
+
+const print = ['C is fun','Python is cool','Javascript is amazing'];
+for (let i in print) {console.log(print[i]); }
