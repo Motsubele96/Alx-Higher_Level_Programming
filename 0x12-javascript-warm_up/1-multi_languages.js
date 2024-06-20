4c8009895acc506f4ac7d8cfe8f6aa9758061cba
root <motsubelekb@gmail.com>
2024-06-20 03:39:59 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
-#!/user/bin/node
+#!/usr/bin/node
 //console.log('C is fun');
 
 const print = ['C is fun','Python is cool','Javascript is amazing'];
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
-if ( process.argv.length === 2 ) {
-	console.log ('no argument');
-} else if ( process.argv.length === 3) {
-	console.log ('Argument Found');
+
+/* Write a script that prints a message depending of the numbers of
+ * arguments passed 
+*/
+const args = process.argv;
+
+if (args.length <= 2) {
+	console.log('No argument');
+} else if (args.length === 3) {
+	console.log('Argument found');
 } else {
-	console.log ('Arguments Found');
+	console.log ('Arguments found');
 }
